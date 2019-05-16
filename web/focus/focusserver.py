4506c39ba45ba04c718f791518eaedad77f34fb4
Tim van Oostrom <timvanoostrom@users.noreply.github.com>
2019-05-16 14:06:44 +0200

    Add send_file method on download request

--- web/focus/focusserver.py
+++ web/focus/focusserver.py
@@ -7,7 +7,7 @@ The server interprets requests, execute the corresponding action and return JSON
 import logging
 
 from flask import jsonify, request, Response
-from .saml import get_bsn_from_saml_token
+from focus.saml import get_bsn_from_saml_token
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +45,7 @@ class FocusServer:
         return Response(
             'Parameter error: {}'.format(message),
             content_type='text/plain',
-            status=422
+            status=500
         )
 
     @staticmethod
@@ -100,6 +100,7 @@ class FocusServer:
         id = request.args.get('id', None)
         isBulk = request.args.get('isBulk', "false").lower() == "true"
         isDms = request.args.get('isDms', "false").lower() == "true"
+        isDownload = request.args.get('download', "false").lower() == "true"
 
         try:
             bsn = get_bsn_from_saml_token(self._tma_certificate)
@@ -111,10 +112,19 @@ class FocusServer:
                 bsn=bsn,
                 id=id,
                 isBulk=isBulk,
-                isDms=isDms
+                isDms=isDms,
+                isDownload=isDownload
             )
         except Exception as error:
             logger.error("Failed to retrieve document: {}".format(str(error)))
             return self._no_connection_response()
+
+        if isDownload:
+            return send_file(
+                io.BytesIO(document['contents']),
+                mimetype=document['mime_type'],
+                as_attachment=True,
+                attachment_filename=document['file_name']
+            )
 
         return jsonify(document)
