35ea274d3c8a5fc6c7607b9b0adc9ccd0e6951be
Johan Otten <drakonen@drakonen.com>
2020-05-14 13:19:56 +0200

    disable tozo tests again

--- web/tests/test_get_jaaropgaven.py
+++ web/tests/test_get_jaaropgaven.py
@@ -85,50 +85,50 @@ class CombinedApiTest(FlaskTestCase):
                         'url': 'focus/document?id=30364921&isBulk=false&isDms=false'
                     }
                 ],
-                'tozodocumenten': [
-                    {
-                        'datePublished': '2020-03-31T18:59:46+02:00',
-                        'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
-                        'id': '4400000031',
-                        'type': 'E-AANVR-TOZO',
-                        'url': 'focus/document?id=4400000031&isBulk=true&isDms=false'
-                    },
-                    {
-                        'datePublished': '2020-03-26T15:18:44+01:00',
-                        'description': 'Verkorte Aanvraag BBZ',
-                        'id': '4400000024',
-                        'type': 'E-AANVR-KBBZ',
-                        'url': 'focus/document?id=4400000024&isBulk=true&isDms=false'
-                    },
-                    {
-                        'datePublished': '2020-03-18T23:09:58+01:00',
-                        'description': 'Ondernemerscheck zelfstandigen',
-                        'id': '4400000020',
-                        'type': 'E-AANVR-DGB',
-                        'url': 'focus/document?id=4400000020&isBulk=true&isDms=false'
-                    },
-                    {
-                        'datePublished': '2020-03-31T00:21:51+02:00',
-                        'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
-                        'id': '4400000030',
-                        'type': 'E-AANVR-TOZO',
-                        'url': 'focus/document?id=4400000030&isBulk=true&isDms=false'
-                    },
-                    {
-                        'datePublished': '2020-03-22T00:26:10+01:00',
-                        'description': 'Aanvraag BBZ',
-                        'id': '4400000022',
-                        'type': 'E-AANVR-BBZ',
-                        'url': 'focus/document?id=4400000022&isBulk=true&isDms=false'
-                    },
-                    {
-                        'datePublished': '2020-03-31T00:04:34+02:00',
-                        'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
-                        'id': '4400000029',
-                        'type': 'E-AANVR-TOZO',
-                        'url': 'focus/document?id=4400000029&isBulk=true&isDms=false'
-                    }
-                ]
+                # 'tozodocumenten': [
+                #     {
+                #         'datePublished': '2020-03-31T18:59:46+02:00',
+                #         'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
+                #         'id': '4400000031',
+                #         'type': 'E-AANVR-TOZO',
+                #         'url': 'focus/document?id=4400000031&isBulk=true&isDms=false'
+                #     },
+                #     {
+                #         'datePublished': '2020-03-26T15:18:44+01:00',
+                #         'description': 'Verkorte Aanvraag BBZ',
+                #         'id': '4400000024',
+                #         'type': 'E-AANVR-KBBZ',
+                #         'url': 'focus/document?id=4400000024&isBulk=true&isDms=false'
+                #     },
+                #     {
+                #         'datePublished': '2020-03-18T23:09:58+01:00',
+                #         'description': 'Ondernemerscheck zelfstandigen',
+                #         'id': '4400000020',
+                #         'type': 'E-AANVR-DGB',
+                #         'url': 'focus/document?id=4400000020&isBulk=true&isDms=false'
+                #     },
+                #     {
+                #         'datePublished': '2020-03-31T00:21:51+02:00',
+                #         'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
+                #         'id': '4400000030',
+                #         'type': 'E-AANVR-TOZO',
+                #         'url': 'focus/document?id=4400000030&isBulk=true&isDms=false'
+                #     },
+                #     {
+                #         'datePublished': '2020-03-22T00:26:10+01:00',
+                #         'description': 'Aanvraag BBZ',
+                #         'id': '4400000022',
+                #         'type': 'E-AANVR-BBZ',
+                #         'url': 'focus/document?id=4400000022&isBulk=true&isDms=false'
+                #     },
+                #     {
+                #         'datePublished': '2020-03-31T00:04:34+02:00',
+                #         'description': 'Tegemoetkoming Ondernemers en Zelfstandigen',
+                #         'id': '4400000029',
+                #         'type': 'E-AANVR-TOZO',
+                #         'url': 'focus/document?id=4400000029&isBulk=true&isDms=false'
+                #     }
+                # ]
             },
             'status': 'OK'
         }
