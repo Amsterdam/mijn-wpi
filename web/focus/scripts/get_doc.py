04f94e867714a6dbcbb5ea9f9dda3ea44034a814
Johan Otten <drakonen@drakonen.com>
2020-09-30 14:24:08 +0200

    syntax error

--- web/focus/scripts/get_doc.py
+++ web/focus/scripts/get_doc.py
@@ -15,9 +15,10 @@ docid = argv[2]
 isDms = argv[3]
 isBulk = argv[4]
 
+print("Getting doc", bsn, docid, isDms, isBulk)
 
 with focus_connection._client.settings(raw_response=True):
-    raw_doc = focus_connection._client.service.getDocument(id=id, bsn=bsn, isBulk=isBulk, isDms=isDms)
+    raw_doc = focus_connection._client.service.getDocument(id=docid, bsn=bsn, isBulk=isBulk, isDms=isDms)
     content_bytesio = BytesIO(raw_doc.content)
     tree = etree.parse(content_bytesio)
     formatted_xml = etree.tostring(tree, pretty_print=True)
