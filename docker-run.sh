137efe408102fcfe6a1ff86cbf5e3eb88bcc11c1
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-01-21 16:34:56 +0100

    Add export

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,7 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x   # print all commands to the terminal
 
+export UWSGI_MODULE="app.server"
+
 # run uwsgi
 exec uwsgi --ini uwsgi.ini
