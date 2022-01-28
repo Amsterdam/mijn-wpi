12cd32da3c4af744388d5870407c21398433d5b3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-01-28 11:25:23 +0100

    Add service calls

--- scripts/test_wsdl_service.py
+++ scripts/test_wsdl_service.py
@@ -1,7 +1,12 @@
+import json
 from app.config import FOCUS_WSDL
 from app.focus_service_aanvragen import get_client
 
 import operator
+
+from app.focus_service_stadspas_admin import get_stadspas_admin_number
+
+bsn = ""
 
 client = get_client()
 
@@ -19,3 +24,10 @@ for service in client.wsdl.services.values():
             print("method :", operation.name)
             print("  input :", operation.input.signature())
             print("  output:", operation.output.signature())
+
+
+focus_stadspas = client.service.getStadspas(bsn=bsn)
+print(json.dumps(focus_stadspas, indent=4))
+
+e_aanvragen = client.service.EAanvragenTozo(bsn=bsn)
+print(json.dumps(e_aanvragen, indent=4))
