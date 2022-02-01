bdd9154bd178fc325aacad237d9419195a3ddb06
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-02-01 17:06:21 +0100

    Add stadspas fonds test

--- app/test_focus_service_stadspas_admin.py
+++ app/test_focus_service_stadspas_admin.py
@@ -32,6 +32,19 @@ class TestFocusStadspasAdmin(TestCase):
 
         self.assertEqual(result, {"type": "hoofdpashouder", "admin_number": 123123123})
 
+    @patch("app.focus_service_stadspas_admin.get_client")
+    def test_get_stadspas_admin_number_none(self, get_client_mock):
+        mock_client = MockClient(
+            name="getStadspas",
+            response=example_response3,
+        )
+        get_client_mock.return_value = mock_client
+        result = get_stadspas_admin_number("11xx11")
+
+        mock_client.service.getStadspas.assert_called_with(bsn="11xx11")
+
+        self.assertEqual(result, None)
+
 
 example_response = {
     "administratienummer": 123123123,
@@ -119,6 +132,25 @@ example_response2 = {
                 "geldigVan": datetime.datetime(1900, 1, 1, 0, 0),
                 "id": 757447,
                 "soortFonds": 3555,
+            },
+        ]
+    },
+}
+
+
+example_response3 = {
+    "administratienummer": 123123123,
+    "bsn": 12312312399,
+    "fondsen": {
+        "fonds": [
+            {
+                "besluit": "toekenning",
+                "dtbegin": datetime.datetime(2020, 9, 1, 0, 0),
+                "dteinde": datetime.datetime(2021, 8, 31, 0, 0),
+                "geldigTotEnMet": None,
+                "geldigVan": datetime.datetime(1900, 1, 1, 0, 0),
+                "id": 180572,
+                "soortFonds": 123123123,
             },
         ]
     },
