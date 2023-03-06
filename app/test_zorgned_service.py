bd02d1e78dd738868f290c808bc893cc64dda986
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2023-03-06 17:00:03 +0100

    MIJN-5198 - Connect zorgned (#46)
    * fix: setup zorgned connection
    
    * fix: add dep fix test
    
    * test: add tests
    
    * fix: add to req root
    
    * fix: add dpath

---
+++ app/test_zorgned_service.py
@@ -0,0 +1,48 @@
+import json
+from unittest import TestCase
+from unittest.mock import patch
+from app.config import BASE_PATH
+
+from app.zorgned_service import (
+    has_armoede_producten,
+    get_aanvragen,
+    get_clientnummer
+)
+
+class ZorgnedApiMock:
+    status_code = 200
+    response_json = None
+
+    def __init__(self, response_json=None):
+        if isinstance(response_json, str):
+            with open(response_json, "r") as read_file:
+                self.response_json = json.load(read_file)
+        else:
+            self.response_json = response_json
+
+    def json(self):
+        return self.response_json
+
+    def raise_for_status(self):
+        if self.status_code != 200:
+            raise Exception("Request failed")
+
+
+class ZorgnedServiceTest(TestCase):
+
+    @patch("app.zorgned_service.requests.get")
+    def test_get_clientnummer_none_response(self, get_mock):
+        get_mock.return_value = ZorgnedApiMock({"_embedded": {"aanvraag": [{"foo": "bar"}]}})
+
+        clientnummer = get_clientnummer(123)
+        
+        self.assertEqual(clientnummer, None)
+
+    @patch("app.zorgned_service.requests.get")
+    def test_has_armoede_producten(self, get_mock):
+        get_mock.return_value = ZorgnedApiMock(BASE_PATH + "/fixtures/aanvragen.json")
+
+        aanvragen = get_aanvragen(123)
+        has_producten = has_armoede_producten(aanvragen)
+        
+        self.assertEqual(has_producten, True)
---
+++ app/zorgned_service.py
@@ -0,0 +1,95 @@
+import json
+import logging
+import requests
+from dpath import util as dpath_util
+from datetime import date
+from app.config import (
+    SERVER_CLIENT_CERT,
+    SERVER_CLIENT_KEY,
+    ZORGNED_API_REQUEST_TIMEOUT_SECONDS,
+    ZORGNED_API_TOKEN,
+    ZORGNED_API_URL,
+    ZORGNED_GEMEENTE_CODE,
+    BESCHIKT_PRODUCT_RESULTAAT,
+    ARMOEDE_REGELING_PRODUCT_CODES
+)
+from app.utils import to_date
+
+def send_api_request(bsn, operation="", query_params=None):
+    headers = None
+    cert = None
+
+    headers = {"Token": ZORGNED_API_TOKEN}
+    cert = (SERVER_CLIENT_CERT, SERVER_CLIENT_KEY)
+    url = f"{ZORGNED_API_URL}/gemeenten/{ZORGNED_GEMEENTE_CODE}/ingeschrevenpersonen/{bsn}{operation}"
+
+    res = requests.get(
+        url,
+        timeout=ZORGNED_API_REQUEST_TIMEOUT_SECONDS,
+        headers=headers,
+        cert=cert,
+        params=query_params,
+    )
+
+    res.raise_for_status()
+
+    return res
+
+
+def send_api_request_json(bsn, operation="", query_params=None):
+    res = send_api_request(bsn, operation, query_params)
+
+    response_data = res.json()
+
+    logging.debug(json.dumps(response_data, indent=4))
+
+    return response_data
+
+
+def date_in_past(past_date):
+    return (
+        past_date
+        and to_date(past_date) <= date.today()
+    )
+
+
+def has_armoede_producten(aanvragen_source=[]):
+    for aanvraag_source in aanvragen_source:
+        beschikking = dpath_util.get(aanvraag_source, "beschikking", default=None)
+        beschikte_producten = dpath_util.get(
+            beschikking, "beschikteProducten", default=None
+        )
+
+        if beschikte_producten:
+            for beschikt_product in beschikte_producten:
+                product = beschikt_product.get("product")
+                toegewezen_product = beschikt_product.get("toegewezenProduct")
+                # If any one product matches out criteria return True
+                if beschikt_product.get("resultaat") in BESCHIKT_PRODUCT_RESULTAAT and product.get("productsoortCode") in ARMOEDE_REGELING_PRODUCT_CODES and date_in_past(toegewezen_product.get("datumIngangGeldigheid")):
+                    return True
+
+    return False
+
+
+def get_aanvragen(bsn, query_params=None):
+    response_data = send_api_request_json(
+        bsn,
+        "/aanvragen",
+        query_params,
+    )
+    response_aanvragen = response_data["_embedded"]["aanvraag"]
+
+    return response_aanvragen
+
+
+def get_clientnummer(bsn):
+    armoede_aanvragen = get_aanvragen(bsn)
+    if(not has_armoede_producten(armoede_aanvragen)):
+        return None
+
+    response_data = send_api_request_json(
+        bsn,
+        "/persoonsgegevensNAW",
+    )
+
+    return response_data["persoon"]["clientidentificatie"]
