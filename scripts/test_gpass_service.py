f3f793d6ab4ac95f786dfe3f76bef5f02b5edd83
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-03-06 17:14:10 +0100

    Add script

--- scripts/test_gpass_service.py
+++ scripts/test_gpass_service.py
@@ -1,8 +1,11 @@
+import sys
 from app.focus_service_stadspas_admin import get_stadspas_admin_number
 from app.gpass_service import get_stadspassen
 
 
-bsn = ""
+bsn = None
+if len(sys.argv) >= 2:
+    bsn = sys.argv[1]
 
 admin = get_stadspas_admin_number(bsn)
 stadspas = get_stadspassen(admin["admin_number"])
