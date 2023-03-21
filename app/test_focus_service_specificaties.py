611c436b36d199b23cbbe1736c34743137739f55
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-03-21 13:13:23 +0100

    Rm unused

--- app/test_focus_service_specificaties.py
+++ app/test_focus_service_specificaties.py
@@ -1,6 +1,6 @@
 import datetime
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from app.focus_service_specificaties import get_jaaropgaven, get_uitkeringsspecificaties
 from app.test_app import MockClient
