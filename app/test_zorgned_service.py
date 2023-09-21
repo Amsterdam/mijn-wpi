abf1d6d7c11616622bac4379299395b6b072d7cb
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-09-21 15:04:37 +0200

    Add test (#73)

--- app/test_zorgned_service.py
+++ app/test_zorgned_service.py
@@ -9,11 +9,11 @@ from app.zorgned_service import get_clientnummer, volledig_clientnummer
 class ZorgnedApiMock:
     status_code = 200
     response_json = None
-    content = {
-        "mock": "Data"
-    }
+    content = {"mock": "Data"}
 
-    def __init__(self, response_json=None):
+    def __init__(self, response_json=None, status_code=200):
+        self.status_code = status_code
+
         if isinstance(response_json, str):
             with open(response_json, "r") as read_file:
                 self.response_json = json.load(read_file)
@@ -29,7 +29,6 @@ class ZorgnedApiMock:
 
 
 class ZorgnedServiceTest(TestCase):
-
     @patch("app.zorgned_service.requests.get")
     def test_get_clientnummer_response(self, get_mock):
         get_mock.return_value = ZorgnedApiMock(BASE_PATH + "/fixtures/persoon.json")
@@ -38,5 +37,19 @@ class ZorgnedServiceTest(TestCase):
 
         self.assertEqual(clientnummer, 304184)
 
+    @patch("app.zorgned_service.requests.get")
+    def test_get_clientnummer_not_found_response(self, get_mock):
+        get_mock.return_value = ZorgnedApiMock(status_code=404)
+
+        clientnummer = get_clientnummer(123)
+
+        self.assertEqual(clientnummer, None)
+
+    @patch("app.zorgned_service.requests.get")
+    def test_get_clientnummer_bad_request_response(self, get_mock):
+        get_mock.return_value = ZorgnedApiMock(status_code=400)
+
+        self.assertRaises(Exception, get_clientnummer, 123)
+
     def test_volledig_clientnummer(self):
-        self.assertEquals(volledig_clientnummer(304184), "03630000304184")
+        self.assertEqual(volledig_clientnummer(304184), "03630000304184")
