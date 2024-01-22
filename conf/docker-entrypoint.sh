1157bc478bde3605fed7d7b4fc7bb53a4a07d454
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2024-01-22 13:36:10 +0100

    Restore condition

--- conf/docker-entrypoint.sh
+++ conf/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # AZ AppService allows SSH into a App instance.
-if [ "$MA_OTAP_ENV" = "production" ]
+if [ "$MA_OTAP_ENV" = "test" ]
 then
     echo "Starting SSH ..."
     service ssh start
